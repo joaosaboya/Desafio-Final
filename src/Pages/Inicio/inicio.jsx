6cade5578e9c2e9fe37381bbc975cfdc8ef6d98a
joaosaboya <joasaboya123@gmail.com>
2024-11-27 19:38:57 -0300

    adicionei o footer e o queroDoar

--- src/Pages/Inicio/inicio.jsx
+++ src/Pages/Inicio/inicio.jsx
@@ -1,12 +1,39 @@
 import S from './inicio.module.scss'
+import comunidade from '../../assets/comunidade.png'
+import grupo from '../../assets/grupo.png'
+import ler from '../../assets/ler.png'
+import balança from '../../assets/balança.png'
 
 
 export default function Inicio(){
     return(
-        <section>
+        <main>
             <section className={S.post}>
         <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
             </section>
-        </section>
+            <section className={S.devoDoar}>
+                <div>
+                    <h2>Por que devo doar?</h2>
+                </div>
+                <section className={S.boxCard}>
+                    <article>
+                        <img src={grupo} alt="" />
+                        <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+                    </article>
+                    <article>
+                        <img src={ler} alt="" />
+                        <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+                    </article>
+                    <article>
+                        <img src={grupo} alt="" />
+                        <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+                    </article>
+                    <article>
+                        <img src={balança} alt="" />
+                        <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+                    </article>
+                </section>
+            </section>
+        </main>
     )
 }
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,7 +1,27 @@
+import facebook from '../../assets/facebook.png'
+import twitter from '../../assets/Twitter.png'
+import youtube from '../../assets/youtube.png'
+import linkedin from '../../assets/linkedin.png'
+import instagram from '../../assets/instagram.png'
+import S from '../../components/Footer/footer.module.scss'
+
 export default function Footer() {
     return (
       <footer>
-        <h2>Eu sou a Footer</h2>
+        <section className={S.boxSocial}>
+          <h3>4002-8922</h3>
+          <nav>
+            <a href=""><img src={facebook} alt="" /></a>
+            <a href=""><img src={twitter} alt="" /></a>
+            <a href=""><img src={youtube} alt="" /></a>
+            <a href=""><img src={linkedin} alt="" /></a>
+            <a href=""><img src={instagram} alt="" /></a>
+          </nav>
+        </section>
+        <section className={S.boxAlerta}>
+          <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
+
+        </section>
       </footer>
     );
   }
