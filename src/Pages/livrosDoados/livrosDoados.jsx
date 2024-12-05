e12e2a3e25ad665ea0565638b3462b2c410a8605
joaosaboya <joasaboya123@gmail.com>
2024-12-04 22:32:51 -0300

    Segundo Commit

--- src/Pages/livrosDoados/livrosDoados.jsx
+++ src/Pages/livrosDoados/livrosDoados.jsx
@@ -1,10 +1,24 @@
-
-
+import S from './livrosDoados.module.scss'
+import livro from '../../assets/livro_Protagonista.png'
 
 export default function livrosDoados(){
     return(
-        <section>
-            <h2>Eu sou a livros doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxBooks}>
+                <article>
+                <img src={livro} alt="Imagem do livro O protagonista" />
+                <h3>O protagonista</h3>
+                <p>Susanne Andrade</p>
+                <p>Ficção</p>
+                </article>
+                <article>
+                <img src={livro} alt="Imagem do livro O protagonista" />
+                <h3>O protagonista</h3>
+                <p>Susanne Andrade</p>
+                <p>Ficção</p>
+                </article>
+            </section>
         </section>
 
 
