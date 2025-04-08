4c586f8d634529812425effd1d4500eb86cc1f8d
Joao <joaosaboya123@gmail.com>
2025-04-07 22:51:48 -0300

    Implementação API

--- src/Pages/livrosDoados/livrosDoados.jsx
+++ src/Pages/livrosDoados/livrosDoados.jsx
@@ -1,7 +1,24 @@
 import S from './livrosDoados.module.scss'
 import livro from '../../assets/livro_Protagonista.png'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 export default function livrosDoados(){
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        try {
+            const response = await axios.get("https://projeto-api-2-zkp9.onrender.com/livros")
+            setLivros(response.data)    
+        } catch (error) {
+            console.error("Erro ao buscar livro:", error)
+        }
+    }
+
+    useEffect (() =>{
+        getLivros()
+    }, [])
+
     return(
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
@@ -12,12 +29,15 @@ export default function livrosDoados(){
                 <p>Susanne Andrade</p>
                 <p>Ficção</p>
                 </article>
-                <article>
-                <img src={livro} alt="Imagem do livro O protagonista" />
-                <h3>O protagonista</h3>
-                <p>Susanne Andrade</p>
-                <p>Ficção</p>
-                </article>
+
+                {livros.map((item) => (
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt={item.titulo} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </article>
+                ))}
             </section>
         </section>
 
--- src/Pages/queroDoar/queroDoar.jsx
+++ src/Pages/queroDoar/queroDoar.jsx
@@ -1,22 +1,65 @@
 import S from './queroDoar.module.scss'
 import livro from '../../assets/livro.png'
+import axios from 'axios'
+import { useState } from 'react'
 
 
 export default function QueroDoar(){
+
+    const [titulo, setTitulo] = useState ("")
+    const [categoria, setCategoria] = useState ("")
+    const [autor, setAutor] = useState ("")
+    const [imagem_url, setImagem_url] = useState ("")
+
+    const enviarDados = async()=>{
+
+        const urlApi = "https://projeto-api-2-zkp9.onrender.com/doar"
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+
+        await axios.post(urlApi, dadosEnviar)
+
+        alert ("Livro cadastrado!")
+
+        setTitulo("")
+        setAutor("")
+        setCategoria("")
+        setImagem_url("")
+    }
+
+    const capturaTitulo = (e) =>{
+        setTitulo(e.target.value)
+        console.log(e.target.value)
+    }
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+    }
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+    const capturaImagem_url = (e) =>{
+        setImagem_url(e.target.value)
+    }
+
     return(
         <section className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action="">
+                <form onSubmit={(e) => e.preventDefault()}>
                     <div>
                         <img src={livro} alt="Figura de um Livro" />
                         <h3>Informações do Livro</h3>
                     </div>
-                    <input type="text" placeholder='Titulo'/>
-                    <input type="text" placeholder='Categoria'/>
-                    <input type="text" placeholder='Autor'/>
-                    <input type="text" placeholder='Link da Imagem'/>
-                    <input className={S.doar} type="submit" value="Doar" />
+                    <input type="text" placeholder='Titulo' onChange={capturaTitulo} value={titulo}/>
+                    <input type="text" placeholder='Categoria' onChange={capturaCategoria} value={categoria}/>
+                    <input type="text" placeholder='Autor'onChange={capturaAutor} value={autor}/>
+                    <input type="text" placeholder='Link da Imagem' onChange={capturaImagem_url} value={imagem_url}/>
+                    <input className={S.doar} type="submit" value="Doar" onClick={enviarDados}/>
                 </form>
             </section>
         </section>
